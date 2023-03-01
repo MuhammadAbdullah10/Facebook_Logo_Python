392b97eb43156ca720b2ea21618b014c97573faa
MuhammadAbdullah10 <77779415+MuhammadAbdullah10@users.noreply.github.com>
2023-03-01 17:14:40 +0000

    Add files via upload

---
+++ FaceBook Logo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import turtle as t
+s=t.Screen()
+s.bgcolor('white')
+a=t.Turtle()
+a.shape('arrow')
+a.pensize(7)
+a.color('blue')
+a.begin_fill()
+a.circle(140,360)
+a.end_fill()
+a.right(7)
+a.backward(32)
+a.color('white')
+a.pensize(3)
+a.begin_fill()
+a.left(97)
+a.forward(90)
+a.left(90)
+a.forward(40)
+a.right(90)
+a.forward(40)
+a.right(90)
+a.forward(34)
+a.left(90)
+a.forward(40)
+a.circle(-45,90)
+a.forward(40)
+a.right(90)
+a.forward(40)
+a.right(90)
+a.forward(30)
+a.circle(10,90)
+a.forward(32)
+a.left(90)
+a.forward(40)
+a.right(90)
+a.forward(40)
+a.right(90)
+a.forward(40)
+a.left(90)
+a.forward(120)
+a.end_fill()
+a.hideturtle()
+t.done
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
